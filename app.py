34f0ea688b1f86af70b8b48d8b1e9dfa2fd42a37
Marcus Lazaldi <100615569+mlazaldi@users.noreply.github.com>
2025-02-01 14:11:04 -0800

    set debug == false

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ app.register_blueprint(views, url_prefix="/")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=8000)
+    app.run(debug=False, use_reloader=False, host="0.0.0.0", port=8000)
 
 # %%
